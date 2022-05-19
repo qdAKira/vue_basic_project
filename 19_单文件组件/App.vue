a2e331da2d3d63a08c09eb622b679b26730d816c
jinjing <995538274@qq.com>
2022-05-19 17:15:58 +0800

    5/19

---
+++ 19_单文件组件/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <School></School>
+    <Student></Student>
+  </div>
+</template>
+
+<script>
+//引入组件
+import Student from './Student.vue'
+
+export default {
+  name:'App',
+  comments:{
+    School,
+    Student
+  }
+}
+</script>
+
---
+++ 19_单文件组件/main.js
@@ -0,0 +1,2 @@
+import App from './App.vue'
+src
